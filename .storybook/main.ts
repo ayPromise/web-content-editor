a9a156893715f483b429087380402e33cebdd2cd
ayPromise servant <fatherpother@gmail.com>
2025-02-18 00:50:06 +0200

    changed config files that anyway changed nothing

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,11 +4,14 @@ const config: StorybookConfig = {
   stories: [
     "../components/**/*.stories.@(js|jsx|ts|tsx)"
   ],
+  
   addons: [
     "@storybook/addon-onboarding",
+    '@storybook/addon-links',
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    '@storybook/preset-create-react-app'
   ],
   framework: {
     name: "@storybook/nextjs",
