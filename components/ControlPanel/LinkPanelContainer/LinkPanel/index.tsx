c51dccd7086e31dc5b2459bbdebd4d9f0b2a68a4
ayPromise servant <fatherpother@gmail.com>
2025-02-27 17:26:44 +0200

    changed logic of rendering the link panel

--- components/ControlPanel/LinkPanelContainer/LinkPanel/index.tsx
+++ components/ControlPanel/LinkPanelContainer/LinkPanel/index.tsx
@@ -1,4 +1,4 @@
-import { SelectedTextProps, useTextSelection } from "@/context/TextSelectionContext";
+import { useTextSelection } from "@/context/TextSelectionContext";
 import React, { useEffect, useState } from "react";
 
 interface LinkPanelProps {
@@ -7,6 +7,7 @@ interface LinkPanelProps {
 }
 
 const LinkPanel: React.FC<LinkPanelProps> = ({ value, onSubmit }) => {
+    const { selectedText } = useTextSelection()
     const [inputValue, setInputValue] = useState(value);
     const [position, setPosition] = useState<{ top: number; left: number }>({ top: 0, left: 0 });
 
@@ -23,21 +24,33 @@ const LinkPanel: React.FC<LinkPanelProps> = ({ value, onSubmit }) => {
 
     // Отримуємо координати тільки один раз при відкритті
     useEffect(() => {
-        const selection = window.getSelection();
-        if (!selection || selection.rangeCount === 0) return;
+        if (!selectedText) return
 
-        const range = selection.getRangeAt(0);
+        const range = document.createRange();
+        // render panel over the single element selected
+        if (selectedText.fromTextNode) {
+            const { node, startIndex, endIndex } = selectedText.fromTextNode
+            range.setStart(node, startIndex);
+            range.setEnd(node, endIndex);
+        } else if (selectedText.orderedNodes) {
+            // render the panel over the multiple elements selected 
+            const firstObjElement = selectedText.orderedNodes[0]
+            const lastObjElement = selectedText.orderedNodes[selectedText.orderedNodes?.length - 1]
+            range.setStart(firstObjElement.node, firstObjElement.startIndex);
+            range.setEnd(lastObjElement.node, lastObjElement.endIndex);
+        }
+
         const rect = range.getBoundingClientRect();
 
         setPosition({
-            top: rect.top + window.scrollY - 60, // Зсуваємо трохи вище
-            left: rect.left + window.scrollX - rect.width,
+            top: rect.top + window.scrollY - 60,
+            left: rect.left + window.scrollX - 30
         });
     }, []);
 
     return (
         <div
-            className="absolute bg-white rounded shadow-lg p-2 border border-white flex items-center"
+            className="absolute bg-white rounded shadow-lg p-2 border border-black flex items-center"
             style={{
                 top: position.top,
                 left: position.left,
@@ -48,20 +61,20 @@ const LinkPanel: React.FC<LinkPanelProps> = ({ value, onSubmit }) => {
                 type="text"
                 value={inputValue}
                 onChange={handleChange}
-                className="rounded-l py-1 px-2 bg-[#867e57] text-white flex-grow focus:outline-none"
+                className="rounded-l py-1 px-2 border flex-grow focus:outline-none"
                 placeholder="Enter URL"
             />
             <button
                 onClick={handleSubmit}
-                className="px-2 py-1 border border-white text-white rounded-r hover:bg-yellow-700 transition"
+                className="px-2 py-1 border border-black rounded-r bg-[#F39200] hover:bg-[#f39200b8] transition"
             >
                 Submit
             </button>
 
-            <div className="absolute size-3 bg-white rotate-45 border-b border-r border-white"
+            <div className="absolute size-3 bg-white rotate-45 border-b border-r border-black"
                 style={{
                     top: 44,
-                    left: position.left,
+                    left: position.left / 3,
                     zIndex: 1000,
                 }}></div>
         </div>
