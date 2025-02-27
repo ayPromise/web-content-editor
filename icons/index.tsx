27ff023007296110d15a19236d83d750edb2afc0
ayPromise servant <fatherpother@gmail.com>
2025-02-27 17:38:31 +0200

    added new icon

--- icons/index.tsx
+++ icons/index.tsx
@@ -84,5 +84,11 @@ const TextAlignJustifyRightIcon = (props: React.SVGProps<SVGSVGElement>) => (
     </svg>
 );
 
+const MultiplicationSignIcon = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={24} height={24} color={"#000000"} fill={"none"} {...props}>
+        <path d="M18 6L12 12M12 12L6 18M12 12L18 18M12 12L6 6" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>
+);
 
-export { DragDropHorizontalIcon, TextBoldIcon, TextItalicIcon, TextUnderlineIcon, TextStrikethroughIcon, Link02Icon, SourceCodeIcon, ArrowDown01Icon, TextAlignJustifyCenterIcon, TextAlignJustifyLeftIcon, TextAlignJustifyRightIcon }
+
+export { DragDropHorizontalIcon, TextBoldIcon, TextItalicIcon, TextUnderlineIcon, TextStrikethroughIcon, Link02Icon, SourceCodeIcon, ArrowDown01Icon, TextAlignJustifyCenterIcon, TextAlignJustifyLeftIcon, TextAlignJustifyRightIcon, MultiplicationSignIcon }
